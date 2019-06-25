7d5f780bd5f46e0cab29a7451ce058c6d478634f
Saidi Amine <amine.saidi@esprit.tn>
2019-06-25 13:52:13 +0100

    Project Spring Initial

---
+++ src/main/java/lmc/stage/springprojectstage/SpringprojectstageApplication.java
@@ -0,0 +1,13 @@
+package lmc.stage.springprojectstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringprojectstageApplication.class, args);
+    }
+
+}
